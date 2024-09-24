36facfc2a0029642d26eb09ddb6ec51a3ea2afa6
yuyahy <xvto.ya@gmail.com>
2024-09-25 07:32:24 +0900

    fix: reviewdog対応

--- src/entity/lambda.go
+++ src/entity/lambda.go
@@ -2,6 +2,6 @@ package entity
 
 // Response Lambdaが返答するデータ
 type Response struct {
-	RequestMethod string `json:RequestMethod`
-	Result        Item   `json:Result`
+	RequestMethod string `json:"RequestMethod"`
+	Result        Item   `json:"Result"`
 }
